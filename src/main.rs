cda81e5f4b6f0fe582d17a04aad1a3398a329924
mattaroni <MatthewMar@proton.me>
2026-05-29 19:27:58 -0500

    Rename `output` argument to `filename`

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod downloaders;
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Cli {
+    /// rename the downloaded file
+    #[clap(short, long)]
+    filename: Option<String>,
+
+    /// use a streamed downloader, instead of a linear downloader
     #[clap(short, long)]
     stream: bool,
-
-    #[clap(short, long)]
-    output: Option<String>,
 
     url: String,
 }
@@ -17,7 +19,7 @@ struct Cli {
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
-    let filename = args.output.unwrap_or_else(|| get_filename(&args.url));
+    let filename = args.filename.unwrap_or_else(|| get_filename(&args.url));
 
     let result = if args.stream {
         downloaders::streamed_download(args.url, filename).await
