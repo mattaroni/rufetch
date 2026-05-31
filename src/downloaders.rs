7926ed8d886fb1656fb910f476056d59b4aa0dde
mattaroni <MatthewMar@proton.me>
2026-05-31 00:02:02 -0500

    Remove download time message

--- src/downloaders.rs
+++ src/downloaders.rs
@@ -1,24 +1,20 @@
 use std::{error::Error, pin::Pin};
 use futures_util::StreamExt;
 use reqwest::get;
-use tokio::{io::{AsyncWrite, AsyncWriteExt}, time::Instant};
+use tokio::io::{AsyncWrite, AsyncWriteExt};
 
 type Output = Pin<Box<dyn AsyncWrite>>;
 type UnitResult = Result<(), Box<dyn Error>>;
 
 pub async fn linear_download(url: String, mut output: Output) -> UnitResult {
-    let start = Instant::now();
     let content = get(url).await?.bytes().await?;
 
     output.write_all(&content).await?;
     output.flush().await?;
-
-    println!("Downloaded in {:?}", start.elapsed());
     Ok(())
 }
 
 pub async fn streamed_download(url: String, mut output: Output) -> UnitResult {
-    let start = Instant::now();
     let mut stream = get(url).await?.bytes_stream();
 
     while let Some(item) = stream.next().await {
@@ -27,7 +23,5 @@ pub async fn streamed_download(url: String, mut output: Output) -> UnitResult {
     }
 
     output.flush().await?;
-
-    println!("Downloaded in {:?}", start.elapsed());
     Ok(())
 }
