221f64fd362087aa154da42bae3cdcc51e04de5f
Matthew Marshall <MatthewMar@proton.me>
2026-06-06 23:17:10 -0500

    Use buffered writer for streamed downloader

--- src/downloaders.rs
+++ src/downloaders.rs
@@ -1,6 +1,7 @@
 use std::{error, pin::Pin};
+
 use futures_util::StreamExt;
-use tokio::io::{AsyncWrite, AsyncWriteExt};
+use tokio::io::{AsyncWrite, AsyncWriteExt, BufWriter};
 
 type Error = Box<dyn error::Error>;
 type Output = Pin<Box<dyn AsyncWrite>>;
@@ -13,14 +14,14 @@ pub async fn linear_download(url: String, mut output: Output) -> Result<(), Erro
     Ok(())
 }
 
-pub async fn streamed_download(url: String, mut output: Output) -> Result<(), Error> {
+pub async fn streamed_download(url: String, output: Output) -> Result<(), Error> {
+    let mut buffer = BufWriter::new(output);
     let mut stream = reqwest::get(url).await?.bytes_stream();
 
     while let Some(item) = stream.next().await {
-        let chunk = item?;
-        output.write_all(&chunk).await?;
+        buffer.write_all(&item?).await?;
     }
 
-    output.flush().await?;
+    buffer.flush().await?;
     Ok(())
 }
